c29e2058e8691a50a43dcc4da2802b29cec8ef5e
anthonymin <dbstnsdl12@naver.com>
2020-01-04 17:48:36 +0900

    changed url, utterances, google-analytics

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   title: `Yungi's Dev Blog`,
-  description: `Blog posted about ...`,
+  description: `Blog posted about Develop and Daily life`,
   author: `[Anthony min]`,
   introduction: `기술에 관심이 많은 대학생입니다.\n배우는 것을 즐기며 지내고 있습니다.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  siteUrl: `https://anthonymin.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: `anthonyminyungi`, // Your GitHub account
@@ -15,7 +15,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'anthonyminyungi/yungi-gatsby-blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
@@ -26,5 +26,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'UA-155331130-1', // Add your google analytics tranking ID
 }
