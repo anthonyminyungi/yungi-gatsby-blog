683b92ab02b704e2654a91046e04631bc2454911
anthonymin <dbstnsdl12@naver.com>
2020-01-04 17:26:21 +0900

    initial commit

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `Yungi's Dev Blog`,
+  description: `Blog posted about ...`,
+  author: `[Anthony min]`,
+  introduction: `기술에 관심이 많은 대학생입니다.\n배우는 것을 즐기며 지내고 있습니다.`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `anthonyminyungi`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: `dbstnsdl12`, // Your Facebook account
+    instagram: `min_stagrams`,
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
