9c1ee55d393f61bb504264d884149e06bdeee923
anthonymin <dbstnsdl12@naver.com>
2020-01-05 23:32:30 +0900

    replaced resume.md to created Resume component, modified about.js

---
+++ src/components/resume/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './index.scss'
+
+export const Resume = () => {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <h2>Comming Soon...!</h2>
+    </div>
+  )
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,46 +1,64 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { Resume } from '../components/resume'
+import { Layout } from '../layout'
+// import { graphql } from 'gatsby'
 
-import { rhythm } from '../utils/typography'
-import * as Lang from '../constants'
+// import { rhythm } from '../utils/typography'
+// import * as Lang from '../constants'
 
-export default ({ data }) => {
-  const resumes = data.allMarkdownRemark.edges
+export default ({ data, location }) => {
+  const metaData = data.site.siteMetadata
+  const { title } = metaData
+  // const resumes = data.allMarkdownRemark.edges
 
-  const resume = resumes
-    .filter(({ node }) => node.frontmatter.lang === Lang.ENGLISH)
-    .map(({ node }) => node)[0]
+  // const resume = resumes
+  //   .filter(({ node }) => node.frontmatter.lang === Lang.ENGLISH)
+  //   .map(({ node }) => node)[0]
 
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(0.5)} ${rhythm(3 / 4)} ${rhythm(1.5)} ${rhythm(
-          3 / 4
-        )}`,
-      }}
-    >
-      <div dangerouslySetInnerHTML={{ __html: resume.html }} />
-    </div>
+    <Layout location={location} title={title}>
+      <Resume />
+    </Layout>
+    // <div
+    // style={{
+    //   marginLeft: `auto`,
+    //   marginRight: `auto`,
+    //   maxWidth: rhythm(24),
+    //   padding: `${rhythm(0.5)} ${rhythm(3 / 4)} ${rhythm(1.5)} ${rhythm(
+    //     3 / 4
+    //   )}`,
+    // }}
+    // >
+    //   hi!
+    // </div>
   )
 }
 
+// export const pageQuery = graphql`
+//   query {
+//     allMarkdownRemark(filter: { frontmatter: { category: { eq: null } } }) {
+//       edges {
+//         node {
+//           id
+//           excerpt(pruneLength: 160)
+//           html
+//           frontmatter {
+//             title
+//             date(formatString: "MMMM DD, YYYY")
+//             lang
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
 export const pageQuery = graphql`
   query {
-    allMarkdownRemark(filter: { frontmatter: { category: { eq: null } } }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 160)
-          html
-          frontmatter {
-            title
-            date(formatString: "MMMM DD, YYYY")
-            lang
-          }
-        }
+    site {
+      siteMetadata {
+        title
+        author
+        siteUrl
       }
     }
   }
